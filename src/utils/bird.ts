73841d44cd0c70f35478bdfc4ce0833b295ea217
Sammer55 <sammerduarte@hotmail.com>
2024-02-14 13:34:23 -0300

    🔧: add project and game configs

---
+++ src/utils/bird.ts
@@ -0,0 +1,6 @@
+export const BIRD_WIDTH = 34;
+export const BIRD_HEIGHT = 24;
+export const BIRD_SPEED = 2500;
+export const JUMP_HEIGHT = 2;
+export const JUMP_SPEED = 150;
+export const JUMP_DURATION = 200;
---
+++ src/utils/game.ts
@@ -0,0 +1,13 @@
+export const BASE_HEIGHT = 140;
+export const WINDOW_HEIGHT = window.innerHeight;
+export const WINDOW_WIDTH = window.innerWidth;
+
+export const FAIR_PLAY_PIPES = 6;
+export const FAIR_PLAY_PIPE_SIZE = {
+  pipeUp: WINDOW_HEIGHT * 0.2 + "px",
+  pipeDown: WINDOW_HEIGHT * 0.2 + "px",
+};
+
+export const PIPES_GAP = 80;
+export const PIPES_SPACE_BETWEEN = 160;
+export const PIPE_WIDTH = 52;
